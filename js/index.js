bc7b4494dace56db38756518ddd010e3845431ea
Kentaro14 <58055418+Kentaro14@users.noreply.github.com>
2020-07-01 21:16:21 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+    // INITIALIZE DATE PICK BOOT
+    $('.datepicker').datepicker({
+        language: "es",
+        clearBtn: true,
+        format: "dd/mm/yyyy",
+        autoclose: true
+    });
